a2618df5352f8740463fe44a1e93cec3143d4281
Liesdn <dc.lipari@gmail.com>
2023-04-02 11:24:36 -0400

    finish 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+	const newArray = [];
+	array.forEach((item) =>  {
+		if (!removeArgs.includes(item)){
+			newArray.push(item);
+		}
+	});
+	return newArray;
 };
 
 // Do not edit below this line
