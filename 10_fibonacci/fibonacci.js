cbe2933ee7e2a3a43e65588a6f351595b64bb2ea
Liesdn <dc.lipari@gmail.com>
2023-04-02 13:08:26 -0400

    Finish 09 and 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	let a = 0;
+	let b = 1;
+	if(n < 0) return 'OOPS';
+	if(n === 0) return 0;
+	for (let i = 1; i < n; i++){
+		let temp = b;
+		b = b + a;
+		a = temp;
+	}
+	return b;
 };
 
 // Do not edit below this line
